a6bb5024853b6560dbe359a4f044f34c7d7b7340
Di <d.osseyran@hotmail.com>
2022-09-13 21:33:20 +0200

    iteration 3 browser is not dark

--- lab-hello-react/src/App.jsx
+++ lab-hello-react/src/App.jsx
@@ -1,9 +1,20 @@
 
 //import reactLogo from './assets/react.svg'
+import  {useState} from 'react'
+import reactLogo from './assets/react.svg'
 import './App.css'
 function App() {
+  const[count, setCount] = useState(0)
  return (
-    <div className="App">
+    <div className="App"> 
+    <header className='App-header'>
+    <img src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/ironhack-logo-xs.png" style={{height: 50}}/>
+    <img src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/menu-top-xs.png"/>
+    <img src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon1.png"/>
+    <img src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon2.png"/>
+    <img src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon3.png"/>
+    <img src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon4.png"/>
+    </header>
   </div>
   )
 }
